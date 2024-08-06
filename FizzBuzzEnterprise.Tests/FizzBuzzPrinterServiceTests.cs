9d27525a5a49494755f2e2906088eef80309e40c
Alexandra Hepworth <git@catmail.dev>
2024-08-06 13:40:56 +0100

    Further implement still-broken test

--- FizzBuzzEnterprise.Tests/FizzBuzzPrinterServiceTests.cs
+++ FizzBuzzEnterprise.Tests/FizzBuzzPrinterServiceTests.cs
@@ -1,13 +1,27 @@
 ﻿using FizzBuzzEnterprise.Services.FizzBuzzPrinter;
+using FizzBuzzEnterprise.Services.OutputWriter;
+using Moq;
 
 namespace FizzBuzzEnterprise.Tests;
 
 public class FizzBuzzPrinterServiceTests
 {
+    private IOutputWriterService? _outputWriter;
+    // We know this will always be set via the SetUp method.
+    private IOutputWriterService OutputWriter => _outputWriter!;
+
+    [SetUp]
+    public void Setup()
+    {
+        // It's a bit of a stretch to define this here instead of in the individual tests, but 
+        // I'm treating this as a dependency whose configuration is not interesting to the tests.
+        _outputWriter = Mock.Of<IOutputWriterService>();
+    }
+    
     [Test]
     public void Can_Instantiate_FizzBuzzPrinterService()
     {
-        Assert.DoesNotThrow(() => { _ = new FizzBuzzPrinterService(); });
+        Assert.DoesNotThrow(() => { _ = new FizzBuzzPrinterService(OutputWriter); });
     }
     
     [TestCase(1, "")]
@@ -17,10 +31,10 @@ public class FizzBuzzPrinterServiceTests
     [TestCase(15, "FizzBuzz")]
     public void FizzBuzzPrinterService_EvaluatesAndPrints_CorrectResult(int number, string expectedString)
     {
-        var service = new FizzBuzzPrinterService();
+        var service = new FizzBuzzPrinterService(OutputWriter);
         service.EvaluateFizzBuzz(number);
         
-        // At this point, we have no way of asserting the output of the console.
-        Assert.Fail("Test is incomplete.");
+        var mock = Mock.Get(OutputWriter);
+        mock.Verify(x => x.Write(expectedString), Times.Once);
     }
 }
--- FizzBuzzEnterprise/Services/FizzBuzzPrinter/FizzBuzzPrinterService.cs
+++ FizzBuzzEnterprise/Services/FizzBuzzPrinter/FizzBuzzPrinterService.cs
@@ -1,7 +1,16 @@
-﻿namespace FizzBuzzEnterprise.Services.FizzBuzzPrinter;
+﻿using FizzBuzzEnterprise.Services.OutputWriter;
+
+namespace FizzBuzzEnterprise.Services.FizzBuzzPrinter;
 
 public class FizzBuzzPrinterService : IFizzBuzzPrinterService
 {
+    private readonly IOutputWriterService _outputWriter;
+
+    public FizzBuzzPrinterService(IOutputWriterService outputWriter)
+    {
+        _outputWriter = outputWriter;
+    }
+    
     public void EvaluateFizzBuzz(int number)
     {
         throw new NotImplementedException();
