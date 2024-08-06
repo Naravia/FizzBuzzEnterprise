73a5cb1a8fbf47a64bd6f7b68da1e59753258129
Alexandra Hepworth <git@catmail.dev>
2024-08-06 12:51:33 +0100

    Add empty tests project

---
+++ FizzBuzzEnterprise.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace FizzBuzzEnterprise.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
