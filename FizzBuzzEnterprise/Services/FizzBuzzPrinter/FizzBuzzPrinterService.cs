7429d08c44fbe97bb8a19f0ba57330221dfc1b83
Alexandra Hepworth <git@catmail.dev>
2024-08-06 13:46:44 +0100

    Implement the FizzBuzzPrinter service evaluate method

--- FizzBuzzEnterprise/Services/FizzBuzzPrinter/FizzBuzzPrinterService.cs
+++ FizzBuzzEnterprise/Services/FizzBuzzPrinter/FizzBuzzPrinterService.cs
@@ -1,10 +1,12 @@
-﻿using FizzBuzzEnterprise.Services.OutputWriter;
+﻿using System.Text;
+using FizzBuzzEnterprise.Services.OutputWriter;
 
 namespace FizzBuzzEnterprise.Services.FizzBuzzPrinter;
 
 public class FizzBuzzPrinterService : IFizzBuzzPrinterService
 {
     private readonly IOutputWriterService _outputWriter;
+    private readonly StringBuilder _output = new("FizzBuzz".Length);
 
     public FizzBuzzPrinterService(IOutputWriterService outputWriter)
     {
@@ -13,6 +15,22 @@ public class FizzBuzzPrinterService : IFizzBuzzPrinterService
     
     public void EvaluateFizzBuzz(int number)
     {
-        throw new NotImplementedException();
+        _output.Clear();
+        if (number % 3 == 0)
+        {
+            _output.Append("Fizz");
+        }
+
+        if (number % 5 == 0)
+        {
+            _output.Append("Buzz");
+        }
+        
+        // This will write an empty string if neither condition is met.
+        // This is a deliberate design decision made for the following reasons:
+        //      1. It makes it easy to detect that "no output" was written. This is useful for testing.
+        //      2. It allows for concatenating empty results e.g. for display via table.
+        //      3. If this behaviour is not desired, it can easily be filtered by the caller.
+        _outputWriter.Write(_output.ToString());
     }
 }
